c05fe94e698d356008175ed670d04dc899d574f1
Akshit <Gakshit92@gmail.com>
2021-08-29 01:09:13 +0530

    Adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import style from "../Styles/footer.module.css";
+import { RiBookmark2Fill } from "react-icons/ri";
+import { IoMdHome } from "react-icons/io";
+import { IoVideocam } from "react-icons/io5";
+import { RiApps2Fill } from "react-icons/ri";
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const options = [
+    {
+      name: "Home",
+      icon: <IoMdHome className={style.icon} />,
+    },
+    {
+      name: "Browse",
+      icon: <IoVideocam className={style.icon} />,
+    },
+    {
+      name: "Bookmark",
+      icon: <RiBookmark2Fill className={style.icon} />,
+    },
+    {
+      name: "More",
+      icon: <RiApps2Fill className={style.icon} />,
+    },
+  ];
 
-export default Footer
+  return (
+    <div className={style.footer}>
+      {options.map((data, idx) => {
+        return (
+          <div className={style.box}>
+            {data.icon}
+            <p>{data.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Footer;
